c6c7e7824a9c8a4902c99f96d9af94429c0d0dea
sophiacphilips <114261752+sophiacphilips@users.noreply.github.com>
2022-10-05 19:18:06 -0700

    wrote program that converts celsius to fahrenheit

---
+++ temp_convert.py
@@ -0,0 +1,11 @@
+# Author: Sophia Philips
+# GitHub Username: sophiacphilips
+# Date: 10/05/2022
+# This program converts Celsius to Fahrenheit for the user.
+
+print("Please enter a Celsius temperature.")
+cel_1=float(input())
+print("The equivalent Fahrenheit temperature is:")
+print(((9/5)*cel_1)+32)
+
+
